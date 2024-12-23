2ced77ff1b5078b03acb51a43a94d3d785273423
Tessyjames <tessyjames28@gmail.com>
2024-12-23 04:17:42 +0100

    Testing selenium installation

--- test.py
+++ test.py
@@ -6,15 +6,6 @@ browser = webdriver.Chrome()
 browser.get('http://selenium.dev/')
 browser.maximize_window()
 
-# Check the title
-title = browser.title
-print(title)
-
-# Test that the title is correct
-assert "Selenium" in title
-
-# Test what happens with the wrong title
-assert "Selenium123" in title
 
 # Add a delay to keep the brower open
 sleep(10)
