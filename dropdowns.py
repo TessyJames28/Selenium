866130d4d9d5469ef3041fb9d1fcbb50d3b800d8
Tessyjames <tessyjames28@gmail.com>
2024-12-24 12:31:26 +0100

    Simple test case for dropdown

---
+++ dropdowns.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.webdriver.support.select import Select
+
+browser = webdriver.Chrome()
+browser.maximize_window()
+login_url = "https://the-internet.herokuapp.com/dropdown"
+browser.get(login_url)
+
+# To interact with the dropdown menu, we need to interact with the select element
+dropdown_element = browser.find_element(By .ID, value="dropdown") #dropdown element
+select = Select(dropdown_element)
+sleep(3)
+
+# Select value by visible text
+# Select the alue by index
+# Select the option by using a value
+
+# select.select_by_visible_text("Option 2")
+# select.select_by_index(1) 
+# select.select_by_value("2")
+
+# Testing the option count: Note that the subheading on the option is part of the index
+option_count = len(select.options)
+expected_count = 3
+if option_count == expected_count:
+    print("Test case passed: count is correct")
+else:
+    print("Test case failed: count is incorrect")
