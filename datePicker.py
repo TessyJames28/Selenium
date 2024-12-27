f6ad57034464e7556cc77dda7e85705fd46c3af2
Tessyjames <tessyjames28@gmail.com>
2024-12-27 12:30:16 +0100

    Handle a standard date picker

--- datePicker.py
+++ datePicker.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timedelta
+from selenium.webdriver import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+
 
 
 url = "https://www.globalsqa.com/demo-site/datepicker/"
@@ -9,5 +12,21 @@ browser.maximize_window()
 browser.get(url)
 
 #Switching to frame to interact with the date picker
-browser.find_element(By .XPATH, value="//div[@class='single_tab_div resp-tab-content resp-tab-content-active']//div[@class='attention closable'][normalize-space()='Pick a date by clicking on the text box.']").click()
+browser.find_element(By .CSS_SELECTOR, value="div[class='single_tab_div resp-tab-content resp-tab-content-active'] a[class='close_img']").click() #Close a popup instruction
 frame_value = browser.find_element(By .XPATH, value="//iframe[@class='demo-frame lazyloaded']")
+browser.switch_to.frame(frame_value)
+sleep(5)
+
+#Identify date picker
+browser.find_element(By .CSS_SELECTOR, value="#datepicker").click()
+
+#Handle the date picker by using the python proper date and time to get the current date
+current_date = datetime.now() # Get the current date and time
+next_date = current_date + timedelta(days=-7) #Use timedelta to select past or future date with '-' used for past date
+
+formatted_date = next_date.strftime("%m/%d/%y") #Put the date in the correct format as on the website
+
+browser.find_element(By .CSS_SELECTOR, value="#datepicker").send_keys(formatted_date + Keys.TAB) #Set the date and click tab to select
+sleep(5)
+
+browser.quit()
