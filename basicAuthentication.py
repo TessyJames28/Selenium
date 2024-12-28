d72ddd33c93a5415ffba95151660a20ddeabf9b0
Tessyjames <tessyjames28@gmail.com>
2024-12-28 12:30:25 +0100

    Handling basic authentication

--- basicAuthentication.py
+++ basicAuthentication.py
@@ -4,7 +4,13 @@ from selenium.webdriver import ActionChains
 from time import sleep
 
 
-url = ""
+# Handling basic auth with no htnl element to identify attributes to use and automate the process
+# To handle the certification, you take the format below as shown on url
+# https://username:password@domain/path
+url = "https://admin:admin@the-internet.herokuapp.com/basic_auth"
+username = "admin" # User name for login
+password = "admin"
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get(url)
+sleep(5)
